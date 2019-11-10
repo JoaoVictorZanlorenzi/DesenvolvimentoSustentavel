18839c381482ff15cac422a4aa974bb3ee3c9372
Joao <joaovzanlorenzi@gmail.com>
2019-11-10 17:50:52 -0300

    Ultima tela e alteracoes

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -46,6 +46,15 @@ const routes: Routes = [
               import('../tab4/tab4.module').then(m => m.Tab4PageModule)
           }
         ]
+      }, {
+        path: 'tab5',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../tab5/tab5.module').then(m => m.Tab5PageModule)
+          }
+        ]
       },
       {
         path: '',
