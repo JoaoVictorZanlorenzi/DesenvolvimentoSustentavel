5d599f11be3f26a37efd5ff45ffc4e4c10e432c6
unknown <biel.guerra@hotmail.com>
2019-11-10 03:40:49 -0300

    Funcionalidade de cadastro, controles da tela e inicio da funcionalidade de login

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+ selector: 'app-tab1',
+ templateUrl: 'tab1.page.html',
+ styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
